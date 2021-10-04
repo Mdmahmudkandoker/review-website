0b7a50bac82fdf0214584b58ffb427040989c564
Mdmahmudkandoker <79156985+Mdmahmudkandoker@users.noreply.github.com>
2021-10-04 22:08:36 +0600

    react

---
+++ src/components/header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { NavLink } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'left' }}>
+            Mahmud Institute
+          </Typography>
+            <Button><NavLink exact activeClassName='active' to='/'>Home</NavLink></Button>
+            <Button><NavLink activeClassName='active' to='/services'>Services</NavLink></Button>
+            <Button> <NavLink activeClassName='active' to='/about'>About</NavLink></Button>
+            <Button><NavLink activeClassName='active' to='/contact'>Contact</NavLink></Button>
+            
+           
+            
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+
+
+
